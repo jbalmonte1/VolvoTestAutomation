ad56641cb4c143e00e7127dc790aa4abbd6364c2
Jason Almonte <jbalmonte1@gmail.com>
2021-10-21 22:03:01 +0800

    Finalized execution

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#rm -rfv allure-results
+if [ -d "allure-results" ]
+then
+	rm -rfv allure-results
+fi
 npx wdio wdio.conf.js --spec './test/specs/**/'${1}'*.js'
 allure generate allure-results --clean
 allure open
